a4333df727df8b761aab189f4eabc349445ef5f6
tianzhisheng <1041218129@qq.com>
2017-12-21 19:30:39 +0800

    测试java api 操作es，实现index/type中的数据增删改查

---
+++ README.md
@@ -0,0 +1,5 @@
+### ElasticSearch 学习
+
+1、ElasticSearch 强大的 Rest API
+
+2、Java API 操作 ElasticSearch
---
+++ src/main/java/com/zhisheng/es/score/first/EmployeeCRUDApp.java
@@ -0,0 +1,102 @@
+package com.zhisheng.es.score.first;
+
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+
+/**
+ * 员工增删改查的应用程序
+ *
+ * @author zhisheng_tian
+ */
+public class EmployeeCRUDApp {
+
+    @SuppressWarnings({"unchecked", "resource"})
+    public static void main(String[] args) throws Exception {
+        // 先构建client
+        Settings settings = Settings.builder()
+                .put("cluster.name", "zhisheng")
+                .build();
+
+        TransportClient client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.153.135"), 9300));
+
+//      createEmployee(client);
+//		getEmployee(client);
+//		updateEmployee(client);
+//		deleteEmployee(client);
+
+        client.close();
+    }
+
+    /**
+     * 创建员工信息（创建一个document）
+     *
+     * @param client
+     */
+    private static void createEmployee(TransportClient client) throws Exception {
+        IndexResponse response = client.prepareIndex("company", "employee", "1")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("name", "zhisheng")
+                        .field("age", 18)
+                        .field("position", "shanghai")
+                        .field("country", "china")
+                        .field("join_date", "2017-01-01")
+                        .field("salary", 10000)
+                        .endObject())
+                .get();
+        System.out.println(response.getResult());
+    }
+
+    /**
+     * 获取员工信息
+     *
+     * @param client
+     * @throws Exception
+     */
+    private static void getEmployee(TransportClient client) throws Exception {
+        GetResponse response = client.prepareGet("company", "employee", "1").get();
+        System.out.println(response.getSourceAsString());
+    }
+
+    /**
+     * 修改员工信息
+     *
+     * @param client
+     * @throws Exception
+     */
+    private static void updateEmployee(TransportClient client) throws Exception {
+        UpdateResponse response = client.prepareUpdate("company", "employee", "1")
+                .setDoc(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("position", "technique manager")
+                        .endObject())
+                .get();
+        System.out.println(response.getResult());
+    }
+
+    /**
+     * 删除 员工信息
+     *
+     * @param client
+     * @throws Exception
+     */
+    private static void deleteEmployee(TransportClient client) throws Exception {
+        DeleteResponse response = client.prepareDelete("company", "employee", "1").get();
+        System.out.println(response.getResult());
+    }
+
+
+
+
+
+}
